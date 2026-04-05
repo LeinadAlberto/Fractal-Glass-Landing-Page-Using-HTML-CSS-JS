539d2a3adbefd565097ed779c0832b1bc786f107
Alberto <dany.canaviri2022@gmail.com>
2026-04-04 22:54:37 -0400

    GSAP Animation, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,16 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.defaults({opacity: 0, ease: 'power4.out', duration: 1.4})
+
+const tl = gsap.timeline()
+const splitTitle = SplitText.create('.home__title', {type: 'chars'})
+
+tl.from('.home__box', {duration: .2}, '.3')
+  .from('.home__bg', {scale: .5}, '.6')
+  .from('.nav > *', {y: -30}, '1.5')
+  .from('.home__icon', {y: 90}, '1.8')
+  .from(splitTitle.chars, {y: 30, autoAlpha: 0, stagger: 0.05}, '2.1')
+  .from('.home__description', {y: 90}, '3.3')
+  .from('.home__buttons', {y: 90}, '3.6')
+  .to('.home__bg', {scale: 1.5, opacity: 1, duration: 8, ease: 'power1.inOut', repeat: -1, yoyo: true, transformOrigin: 'center center'})
